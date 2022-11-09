0cc061767536deeb8e7d8e0fca6ce4db57bb1151
User <bashkatovr@mail.ru>
2022-11-09 20:21:00 +0300

    Variable Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 3
-        System.out.println("Задание 3");
-        var dog = 8.0;
-        System.out.println(dog);
-        dog = dog - 3.5;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
-        paper = paper - 7639;
-        System.out.println(paper);
+        // Задание 4
+        System.out.println("Задание 4");
+        var friend  = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
     }
 }
