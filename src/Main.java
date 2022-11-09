959b8ed200c0975cf783e18b59bf8ec517dab0c0
User <bashkatovr@mail.ru>
2022-11-09 21:03:18 +0300

    Variable Задание 7

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 6
-        System.out.println("Задание 6");
+        // Задание 7
+        System.out.println("Задание 7");
         var boxer1   = 78.2;
         System.out.println(boxer1);
         var boxer2   = 82.7;
         System.out.println(boxer2);
-        var totalWeight = boxer1 + boxer2;
-        System.out.println(totalWeight);
+        var totalWeigt = boxer1 + boxer2;
+        System.out.println(totalWeigt);
         var weightDifference = boxer2 - boxer1;
         System.out.println(weightDifference);
+        var weightDifference1 = (boxer2 - boxer1) % totalWeigt;
+        System.out.println(weightDifference1);
     }
 }
