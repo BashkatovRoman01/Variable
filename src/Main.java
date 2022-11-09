f970eb30f7c1b71f7842d359a674973186a51e78
User <bashkatovr@mail.ru>
2022-11-09 22:07:32 +0300

    Variable Задание 8

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 7
-        System.out.println("Задание 7");
-        var boxer1   = 78.2;
-        System.out.println(boxer1);
-        var boxer2   = 82.7;
-        System.out.println(boxer2);
-        var totalWeigt = boxer1 + boxer2;
-        System.out.println(totalWeigt);
-        var weightDifference = boxer2 - boxer1;
-        System.out.println(weightDifference);
-        var weightDifference1 = (boxer2 - boxer1) % totalWeigt;
-        System.out.println(weightDifference1);
+        // Задание 8
+        System.out.println("Задание 8");
+        var totalHours = 640;
+        var hoursPerDay = 8;
+        var totalEmloyees = totalHours / hoursPerDay;
+        System.out.println("Всего работников в компании – " + totalHours / hoursPerDay + " человек");
+        totalEmloyees = totalEmloyees + 94;
+        var totalWorkingHours = totalEmloyees * hoursPerDay;
+        System.out.println("Если в компании работает " + totalEmloyees + " человек, то всего " + totalWorkingHours + " часов работы может быть поделено между сотрудниками");
+
+
     }
 }
